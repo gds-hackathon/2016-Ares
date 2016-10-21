19518d16bca7a35f0654913a32eff2a1387d2dbc
weiwei <weiwei@WAYNE-MAC>
2016-10-21 14:38:07 +0800

    Initialize solution

---
+++ Ares.Web.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ares.Web.Admin.Startup))]
+namespace Ares.Web.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
