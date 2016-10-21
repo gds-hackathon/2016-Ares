d1b13a2505154dd35fd96572e017d5b4bddd6eb7
oz <blackbat9@live.cn>
2016-10-21 21:52:24 +0800

    Login/logout done

--- Ares.Web.Admin/Controllers/LoginController.cs
+++ Ares.Web.Admin/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Ares.Web.Admin.Controllers
 {
@@ -20,21 +21,46 @@ namespace Ares.Web.Admin.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            var cookie = HttpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (cookie != null && !string.IsNullOrEmpty(cookie.Value))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
         [HttpPost]
         public ActionResult Login(string userName, string password)
         {
-            _accountManager.Login(userName, password);
-            // TODO: add things to coookies
+            var loginResult = _accountManager.Login(userName, password);
+            FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                    1,
+                    userName,
+                    DateTime.Now,
+                    DateTime.Now.AddMinutes(3),
+                    false,
+                    loginResult.RoleType.ToString()
+                    );
+            string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+            HttpCookie authCookie = new System.Web.HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+
+            HttpContext.Response.Cookies.Add(authCookie);
+            HttpContext.Response.Cookies.Add(new HttpCookie("userid", loginResult.UserId.ToString()));
+            HttpContext.Response.Cookies.Add(new HttpCookie("roletype", loginResult.RoleType.ToString()));
             return RedirectToAction("Index", "Home");
         }
 
         public ActionResult Logout()
         {
-            // TODO: remove things from cookies
-            return RedirectToAction("Login");
+            FormsAuthentication.SignOut();
+            HttpCookie c = Request.Cookies[FormsAuthentication.FormsCookieName];
+            c.Expires = DateTime.Now.AddDays(-1);
+
+            Response.Cookies.Set(c);
+
+            Session.Clear();
+            return RedirectToAction("Index");
         }
 
     }
