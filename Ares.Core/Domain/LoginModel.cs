07417e2dd51eecf2f354a63cb12afbdd1e66add9
oz <blackbat9@live.cn>
2016-10-21 17:23:17 +0800

    add login controller and login domain model

---
+++ Ares.Core/Domain/LoginModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ares.Core.Domain
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "RememberMe")]
+        public bool RememberMe { get; set; }
+    }
+}
