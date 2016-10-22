9c2f147a3769228042974e2ad4e78211a8b738b7
oz <blackbat9@live.cn>
2016-10-22 11:17:50 +0800

    add registry

---
+++ Ares.Core/Domain/RegistryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ares.Core.Domain
+{
+    public class RegistryModel
+    {
+        [DisplayName("Login Name")]
+        public string LoginName { get; set; }
+        [DisplayName("Phone Number")]
+        public string PhoneNum { get; set; }
+        [DisplayName("Password")]
+        public string Password { get; set; }
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [DisplayName("Role Type")]
+        public string RoleType { get; set; }
+    }
+}
--- Ares.Web.Admin/Controllers/RestaurantController.cs
+++ Ares.Web.Admin/Controllers/RestaurantController.cs
@@ -13,13 +13,16 @@ namespace Ares.Web.Admin.Controllers
     {
         private ITransactionManager _txnManager;
         private IUserManager _userManager;
+        private IAccountManager _accountManager;
 
         public RestaurantController(
             ITransactionManager txnManager,
-            IUserManager userManager)
+            IUserManager userManager,
+            IAccountManager accountManager)
         {
             _txnManager = txnManager;
             _userManager = userManager;
+            _accountManager = accountManager;
         }
 
         // GET: Restaurant
@@ -57,6 +60,22 @@ namespace Ares.Web.Admin.Controllers
                 txnModels.Add(t);
             }
             return View(txnModels);
+        }
+
+        public ActionResult Registry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registry(RegistryModel cu)
+        {
+            if (!string.IsNullOrEmpty(cu.LoginName))
+            {
+                _accountManager.RegistryUser(cu.LoginName, cu.PhoneNum, cu.Password, cu.Name, RoleTypes.Customer);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
